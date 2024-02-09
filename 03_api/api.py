49abbb9be7982e1b9083c185cf15810140567cf2
Stephen Flynn <stephenflynn@me.com>
2024-02-08 23:26:05 -0500

    upgraded openai client, added clickable demo link

--- 03_api/api.py
+++ 03_api/api.py
@@ -1,8 +1,9 @@
-import openai
 import os
 from fastapi import FastAPI
-from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from openai import OpenAI
+from dotenv import load_dotenv
 
 class Suggestion(BaseModel):
     label: str
@@ -10,6 +11,7 @@ class Suggestion(BaseModel):
 
 # Uses ChatGPT to offer troubleshooting suggestions based on sound & location
 def troubleshoot_car(sound: str, location: str) -> list[Suggestion]:
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
     prompt = f"""Your goal is to help me troubleshoot a problem with my car.  
 I'm hearing a {sound} near {location}.  
 Suggest 3 ideas for determining the issue.
@@ -27,16 +29,14 @@ description: 40-50 words describing what the issue may be
 """
 
     # Calls ChatGPT 3.5 with the above prompt.
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": prompt},
-        ]
-    )
+    response = client.chat.completions.create(model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": prompt},
+    ])
         
     # Extracts the text content from the response.
-    response_content = response.choices[0].message["content"]
+    response_content = response.choices[0].message.content
 
     # Parses the response into Suggestions and returns them
     return parse_suggestions(response_content)
@@ -65,6 +65,11 @@ def parse_suggestions(text: str) -> list[Suggestion]:
 def create_app() -> FastAPI:
     app = FastAPI()
 
+    @app.on_event("startup")
+    def startup_event():
+        clickable_url = "http://127.0.0.1:8000/search?sound=shuddering&location=wheels"
+        print(f"Try out the API: {clickable_url}")
+
     @app.get("/search")
     def search(sound: str, location: str) -> list[Suggestion]:
         return troubleshoot_car(sound, location)
@@ -72,8 +77,9 @@ def create_app() -> FastAPI:
     return app
 
 # Exit early if the api key is not provided
-openai.api_key = os.getenv("OPENAI_API_KEY")
-if openai.api_key is None:
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+if api_key is None:
     print("Must define environment variable OPENAI_API_KEY")
     exit(1)
 
